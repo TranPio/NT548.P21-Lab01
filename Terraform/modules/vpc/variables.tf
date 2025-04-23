674aefaa4e1109ec4f931ee79d7f6bffb2262d7c
Trần Hoài Phú <phubao789@gmail.com>
2025-04-23 15:19:23 +0700

    Trần Hoài Phú update code modules/vpc

---
+++ Terraform/modules/vpc/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+  description = "Project name for tagging resources."
+  type        = string
+  default     = "nt548-lab01-group10" 
+}
+
+variable "region" {
+  description = "The AWS region to deploy the resources in."
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR block for the VPC."
+    type        = string
+}
+
+variable "public_subnet_cidrs" {
+    description = "List of CIDR blocks for public subnets."
+    type        = list(string)
+  
+}
+
+variable "private_subnet_cidrs" {
+    description = "List of CIDR blocks for private subnets."
+    type        = list(string)
+  
+}
+
+variable "availability_zones" {
+    description = "List of availability zones to use."
+    type        = list(string)
+    //default     = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+  
+}
