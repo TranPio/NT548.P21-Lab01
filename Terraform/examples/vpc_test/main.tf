70b74e15fddc6bd73ef08e1bb3e6c1a0cba4e8cc
Trần Hoài Phú <phubao789@gmail.com>
2025-04-24 21:37:22 +0700

    Trần Hoài Phú fix code background

--- Terraform/examples/vpc_test/main.tf
+++ Terraform/examples/vpc_test/main.tf
@@ -1,47 +1,48 @@
 # --- Terraform/examples/vpc_test/main.tf ---
 
-# Gọi module vpc từ thư mục modules nằm cách đây 2 cấp thư mục
 module "vpc_test_instance" {
-  # Đường dẫn tương đối chính xác từ đây đến module vpc
   source = "../../modules/vpc"
 
-  # --- Cung cấp các giá trị đầu vào cho lần test này ---
-
-  project_name = "nt548-lab01-group10"
-  vpc_cidr     = "192.168.0.0/16"
-
-  public_subnet_cidrs  = ["192.168.1.0/24"]
-  private_subnet_cidrs = ["192.168.2.0/24"]
-  availability_zones  = ["ap-southeast-1a"]
+  project_name         = "nt548-lab01-test"
+  availability_zones   = ["ap-southeast-1a"]
 }
 
-# --- (Tùy chọn) Định nghĩa các output để xem kết quả sau khi apply ---
 output "test_vpc_id" {
-  description = "ID của VPC được tạo ra trong quá trình test"
+  description = "ID of the VPC created during the test."
   value       = module.vpc_test_instance.vpc_id
 }
 
 output "test_vpc_cidr_block" {
-  description = "CIDR block của VPC được tạo ra trong quá trình test"
+  description = "CIDR block of the VPC created during the test."
   value       = module.vpc_test_instance.vpc_cidr_block
 }
 
 output "test_public_subnet_ids" {
-  description = "Danh sách ID của các public subnet được tạo ra trong quá trình test"
+  description = "List of public subnet IDs created during the test."
   value       = module.vpc_test_instance.public_subnet_ids
 }
 
+output "test_public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks created during the test."
+  value       = module.vpc_test_instance.public_subnet_cidrs
+}
+
 output "test_private_subnet_ids" {
-  description = "Danh sách ID của các private subnet được tạo ra trong quá trình test"
+  description = "List of private subnet IDs created during the test."
   value       = module.vpc_test_instance.private_subnet_ids
 }
 
+output "test_private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks created during the test."
+  value       = module.vpc_test_instance.private_subnet_cidrs
+}
+
 output "test_igw_id" {
-  description = "ID của Internet Gateway được tạo ra trong quá trình test"
+  description = "ID of the Internet Gateway created during the test."
   value       = module.vpc_test_instance.internet_gateway_id
 }
 
 output "test_default_sg_id" {
-  description = "ID của Default Security Group cho VPC test"
+  description = "ID of the default security group created during the test."
   value       = module.vpc_test_instance.default_security_group_id
 }
--- Terraform/modules/vpc/variables.tf
+++ Terraform/modules/vpc/variables.tf
@@ -13,23 +13,24 @@ variable "region" {
 variable "vpc_cidr" {
     description = "CIDR block for the VPC."
     type        = string
+    default     = "192.168.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
     description = "List of CIDR blocks for public subnets."
     type        = list(string)
-  
+    default     = [ "192.168.1.0/24" ]
 }
 
 variable "private_subnet_cidrs" {
     description = "List of CIDR blocks for private subnets."
     type        = list(string)
-  
+    default     = [ "192.168.2.0/24" ]
 }
 
 variable "availability_zones" {
     description = "List of availability zones to use."
     type        = list(string)
-    //default     = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+    default     = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
   
 }
